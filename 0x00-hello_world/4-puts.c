25c039d327d5eb4b86d3663010145defaad14002
Ijema <jilado10@gmail.com>
2022-09-08 16:53:58 -0700

    Print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+/**
+ * This code prints 
+ * Programming is like building a multilingual blockof code
+ * */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle" \n);
+	return 0;
+}
