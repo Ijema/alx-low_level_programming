60d2ee50404899e7b9b075cb0bb8f3dfe85cbe99
Ijema <jilado10@gmail.com>
2022-09-10 10:22:27 -0700

    a program that prints all possible combinations of two two-digit numbers.
    The numbers should range from 0 to 99
    
    The two numbers should be separated by a space
    
    All numbers should be printed with two digits. 1 should be printed as 01
    
    The combination of numbers must be separated by comma, followed by a space
    
    The combinations of numbers should be printed in ascending order
    
    00 01 and 01 00 are considered as the same combination of the numbers 0 and 1
    
    You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
    
    You can only use putchar eight times maximum in your code
    
    You are not allowed to use any variable of type char
    
    All your code should be in the main function

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 <= 81; num1++)
+	for (num1 = 0; num1 <= 101; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= 82; num2++)
+		for (num2 = num1 + 1; num2 <= 102; num2++)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 / 10) + '0');
