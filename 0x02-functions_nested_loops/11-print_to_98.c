ff7a910631c210ab6c36c7cf90fbf4e324094674
Ijema <jilado10@gmail.com>
2022-09-14 14:59:17 -0700

    a function that prints all natural numbers from n to 98, followed by a new line.
    Prototype: void print_to_98(int n);
    
    Numbers must be separated by a comma, followed by a space
    
    Numbers should be printed in order
    
    The first printed number should be the number passed to your function
    
    The last printed number should be 98
    
    You are allowed to use the standard library

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,15 +7,25 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	while (n <= 98)
 	{
 		_putchar(n);
 	}
-	else if (n < 98)
+	if (n < 98)
 	{
 		_putchar(',');
 		_putchar(' ');
 		n++;
+	}
+	while (n >= 98)
+	{
+		_putchar(n);
+	}
+	if (n > 98)
+	{
+		_putchar(',');
+		_putchar(' ');
+		n--;
 	}
 }
 
