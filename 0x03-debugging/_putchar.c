c8044c1d849ec8d892671776c6ee42f54ae0271b
Ijema <jilado10@gmail.com>
2022-09-16 04:37:53 -0700

    correct it

---
+++ 0x03-debugging/_putchar.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - _putchar declaration
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 /**
  * Main - entry point
  * Return: Always return 0;
  */
+int _putchar(char c);
+void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+
+#endif /* MAIN.H */
