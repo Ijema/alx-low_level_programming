e754863ecca999e579f991e2cfd2e7cfe05f7113
Ijema <jilado10@gmail.com>
2022-09-16 07:17:44 -0700

    first debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,7 @@
-#include <stdio.h>
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * Main - entry point
- * Return: Always return 0;
- */
-int _putchar(char c);
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
 
-#endif /* MAIN.H */
+#include <stdio.h>
+int _putchar(char c);
+
+#endif
