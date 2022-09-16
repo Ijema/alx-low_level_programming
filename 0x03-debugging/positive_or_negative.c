825062c20efae6146fb139e120ee37bb4e08fb02
Ijema <jilado10@gmail.com>
2022-09-16 05:13:21 -0700

    first commit

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -11,7 +11,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n == 0)
-		_putchar("is zero \n");
+		_putchar("is zero", '\n');
 	if (n > 0)
 		_putchar("is positive \n");
 	if (n < 0)
